b0aa1b33d7bf1f14e38c915f1fe8acfee74111a1
Zhenqian Wang <hywzq@126.com>
2026-09-26 12:15:17 +0200

    chore: refresh Google Scholar metrics

--- assets/metrics-data.js
+++ assets/metrics-data.js
@@ -3,7 +3,7 @@
 window.__SCHOLAR_METRICS__ = {
   "source": "Google Scholar",
   "profile": "https://scholar.google.com/citations?user=tv_6I4YAAAAJ",
-  "updated": "2026-09-26T06:15:16+02:00",
+  "updated": "2026-09-26T12:15:17+02:00",
   "updatedDisplay": "26 September 2026",
   "scholar": {
     "sinceLabel": "Since 2021",
